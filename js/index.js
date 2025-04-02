7a5276bdcb8d42055e154ecb4f3a7a89644dd635
Federico Bosio <federicobosio0209@gmail.com>
2025-04-02 21:56:20 +0200

    changed everything

--- js/index.js
+++ js/index.js
@@ -1,14 +1,21 @@
-document.getElementById("form1").addEventListener("submit", function (event) {
-    event.preventDefault();
-    let submittedPass = document.getElementById("password1").value;
-    let submittedUser = document.getElementById("username1").value;
-    let submittedEmail = document.getElementById("email1").value;
-
-    if (submittedUser&&submittedEmail&&submittedPass === "federico"){
-        alert("Login riuscito!")
-        window.location.href = "https://www.google.com"
-    }
-    else{
-        alert("Login errato!")
-    }
-});
+k = document.getElementById("form1")
+if (k){
+    k.addEventListener("submit", function (event) {
+        event.preventDefault();
+        let submittedPass = document.getElementById("password1").value;
+        let submittedUser = document.getElementById("username1").value;
+        let submittedEmail = document.getElementById("email1").value;
+    
+        if (submittedUser&&submittedEmail&&submittedPass === "federico"){
+            alert("Login riuscito!")
+            window.location.href = "https://www.google.com"
+        }
+        else{
+            alert("Login errato!")
+            window.location.href = "https://parrot.live"
+        }
+    });
+}
+else{
+    console.log("errror getting form1");
+}
